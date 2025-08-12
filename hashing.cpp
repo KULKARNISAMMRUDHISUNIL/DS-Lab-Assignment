b3e453ea7949a4ba372f69b812a20a4adbc83adc
KULKARNISAMMRUDHISUNIL <sammrudhikulkarni2024.comp@mmcoe.edu.in>
2025-08-12 15:19:13 +0530

    hashing.cpp

---
+++ hashing.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout<<"Kulkarni Sammrudhi Sunil\n";
+	cout<<"B24CE1125\n";
+    int n,m;
+    char s;
+    cout << "Enter number of servers: ";
+    cin >> n;
+    m=0;
+    int arr[n];
+    int h_val[n]; 
+    while(1) {
+
+cout<<"Enter '1' to add clients and '0' to Exit:";
+cin>>s;
+    if (s=='1'){ 
+cout << "Enter the client ids:\n";
+        cin >> arr[m];
+        h_val[m] = arr[m] % n;
+        cout << "Server No: " << h_val[m] << " Client id: " << arr[m] << endl;
+        m++;
+    } else if(s=='0'){
+cout<<"Exited";
+break;
+}
+else{
+cout<<"Invalid Input";
+break;
+}
+} return 0;}
